8b74d42a5b29d9d898369547b660335b65dbd076
sujitsajja <sujitsajja@outlook.com>
2016-04-01 19:17:06 -0500

    New Algorithm added

---
+++ KnuthMorrisPratt/src/KnuthMorrisPratt.java
@@ -0,0 +1,19 @@
+/**
+ * To find if the given string contains another given string
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-04-01
+ */
+
+public class KnuthMorrisPratt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
