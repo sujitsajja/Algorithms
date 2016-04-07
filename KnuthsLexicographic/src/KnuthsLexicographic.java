64e14539c8a90488c763e90eebc8af92d78ca499
sujitsajja <sujitsajja@outlook.com>
2016-04-07 17:32:38 -0500

    New algorithm added

---
+++ KnuthsLexicographic/src/KnuthsLexicographic.java
@@ -0,0 +1,20 @@
+/**
+ * Find lexicographically next biggest number
+ * formed by the digits from the given number
+ * 
+ * @author SujitS
+ * 
+ * @version 1.0
+ * @since 2016-04-07
+ */
+
+public class KnuthsLexicographic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
